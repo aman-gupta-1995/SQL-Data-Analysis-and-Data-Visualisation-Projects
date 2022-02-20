9e925dc5997235c4dff0db4ea2538c96e8f15167
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-02-21 01:07:10 +0530

    Update 03.Comparison.sql

--- Advanced SQL for Data Science - Time Series/03.Time Series Analysis/03.Comparison.sql
+++ Advanced SQL for Data Science - Time Series/03.Time Series Analysis/03.Comparison.sql
@@ -2,7 +2,7 @@
 
 -- will compare the avg temp of current day with avg temp of previous day
 -- based on this, we can do comparison of month to month, year to year, etc.
-WITH avg_daily_temp AS(
+WITH avg_daily_temp AS( 
 	SELECT  
 		DATE_TRUNC('day', event_time) AS event_day,
 		AVG(temp_celcius) AS avg_temp 

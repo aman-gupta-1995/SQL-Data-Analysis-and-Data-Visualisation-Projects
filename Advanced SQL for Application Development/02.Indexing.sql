9e79da012f555f3520b75f1617c80ef1d7c511f8
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-05-31 23:24:13 +0530

    Update 02.Indexing.sql

--- Advanced SQL for Application Development/02.Indexing.sql
+++ Advanced SQL for Application Development/02.Indexing.sql
@@ -1,6 +1,6 @@
 -- B tree index
 CREATE INDEX idx_last_name_first_name_customers
-ON customers (last_name, first_name);
+ON customers (last_name, first_name); 
 
 -- Hash index
 CREATE INDEX idx_product_name_products

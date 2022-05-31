6e2e3c7dca3e6aebb9cd8fbd5ebf7f4a187afa35
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-05-31 23:24:36 +0530

    Update 02.Indexing.sql

--- Advanced SQL for Application Development/02.Indexing.sql
+++ Advanced SQL for Application Development/02.Indexing.sql
@@ -3,6 +3,6 @@ CREATE INDEX idx_last_name_first_name_customers
 ON customers (last_name, first_name); 
 
 -- Hash index
-CREATE INDEX idx_product_name_products 
+CREATE INDEX idx_product_name_products  
 ON products USING hash(product_name);   
   

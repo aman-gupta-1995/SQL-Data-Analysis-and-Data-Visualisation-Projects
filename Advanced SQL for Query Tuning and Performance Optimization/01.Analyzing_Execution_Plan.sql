69cb82c7dc4ff2563d9374e15f75603b4b5f7dd6
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-05-31 23:27:56 +0530

    Update 01.Analyzing_Execution_Plan.sql

--- Advanced SQL for Query Tuning and Performance Optimization/01.Analyzing_Execution_Plan.sql
+++ Advanced SQL for Query Tuning and Performance Optimization/01.Analyzing_Execution_Plan.sql
@@ -5,7 +5,7 @@ EXPLAIN won't actually execute query. Instead it try to make estimation.
 */
 EXPLAIN SELECT * FROM staff;
  
-/* get details time to build the execution plan and explaination */
+/* get details time to build the execution plan and explaination */ 
 EXPLAIN ANALYZE SELECT * FROM staff; 
 
 /* now width is 7, which is lower than previous one*/

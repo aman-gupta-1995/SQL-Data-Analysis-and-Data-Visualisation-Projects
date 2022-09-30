63f8fad5b2e576ffc5d8da902f4073266d60f959
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-09-30 18:11:31 +0530

    Update 02.Lag.sql

--- Advanced SQL for Data Science - Time Series/02.Commonly used Functions for Time Series/02.Lag.sql
+++ Advanced SQL for Data Science - Time Series/02.Commonly used Functions for Time Series/02.Lag.sql
@@ -5,7 +5,7 @@
 -- LAG() looks backwards and allows us to compare condition with the previous nth row of current row.
  
 SELECT dept_id, server_id, cpu_utilization,  
-	LAG(cpu_utilization) OVER (PARTITION BY dept_id ORDER BY cpu_utilization DESC)
+	LAG(cpu_utilization) OVER (PARTITION BY dept_id ORDER BY cpu_utilization DESC) 
 FROM time_series.vw_utilization
 WHERE event_time BETWEEN '2019-03-05' AND '2019-03-06';
 
